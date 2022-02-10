4303074168aa048cd90dbd3d49d547775b1a7de3
unknown <qangeldratsch@gmail.com>
2022-02-10 11:20:20 +0100

    checked syntax and format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let eingabe = document.querySelector("#berechnen")
-let ergebnis = document.querySelector("#monatsrate")
 let ratendiv = document.querySelector("#monatsrate")
+
 function index(){
     eingabe.addEventListener("click", artauswahl)
 }
@@ -8,44 +8,43 @@ function index(){
 function artauswahl(){
     ratendiv.innerHTML = ""
 
-    let auswahl = document.querySelector("#darlehensart").value
+   let auswahl = document.querySelector("#darlehensart").value
 
     if (auswahl === "annuitaetendarlehen"){
-        annuitaetendarlehen()
+        annuitaetenDarlehen()
     }
     else if (auswahl === "tilgungsdarlehen") {
-        tilgungsdarlehen()
+        tilgungsDarlehen()
     }
-
     else if (auswahl === "faelligkeitsdarlehen") {
-        faelligskeitsdarlehen()
+        faelligskeitsDarlehen()
     }
 }
 
-function annuitaetendarlehen(){
+function annuitaetenDarlehen(){
     let laufzeit = parseInt(document.getElementById("laufzeit").value)
     let zinssatz = parseInt(document.querySelector("#zinssatz").value)
     let summe = parseInt(document.querySelector("#summe").value)
     let zinsen = zinssatz / 100
     let monatsrate = (summe * (((1 + (zinsen / 12)) ** (laufzeit) * zinsen) / ((1 + (zinsen / 12)) ** (laufzeit) - 1))) / 12
-    ergebnis.innerText = monatsrate.toFixed(2) + "€"
+    ratendiv.innerText = monatsrate.toFixed(2) + "€"
 }
 
-function tilgungsdarlehen() {
+function tilgungsDarlehen() {
     let laufzeit = parseInt(document.getElementById("laufzeit").value)
     let zinssatz = parseInt(document.querySelector("#zinssatz").value)
     let summe = parseInt(document.querySelector("#summe").value)
     let zinsen = zinssatz / 100
-    let festerate = summe / laufzeit
+    let festeRate = summe / laufzeit
     let rate = 0
-    let tilgungohnezinsen = document.createElement("h3")
-    tilgungohnezinsen.innerText = "Feste Tilgungsrate ohne Zinsen: " + festerate.toFixed(2) + " €"
+    let tilgungOhneZinsen = document.createElement("h3")
+    tilgungOhneZinsen.innerText = "Feste Tilgungsrate ohne Zinsen: " + festeRate.toFixed(2) + " €"
     ratendiv.appendChild(tilgungohnezinsen)
     let x = 1
-    while (summe > 0) {
 
-        rate = (summe * (zinsen / laufzeit)) + festerate
-        summe = summe - festerate
+    while (summe > 0) {
+        rate = (summe * (zinsen / laufzeit)) + festeRate
+        summe = summe - festeRate
         let zahlung = document.createElement("div")
         zahlung.innerText = x + ". Rate: " + rate.toFixed(2) + " €"
         ratendiv.appendChild(zahlung)
@@ -53,7 +52,7 @@ function tilgungsdarlehen() {
     }
 }
 
-function faelligskeitsdarlehen(){
+function faelligskeitsDarlehen(){
     let laufzeit = parseInt(document.getElementById("laufzeit").value)
     let zinssatz = parseInt(document.querySelector("#zinssatz").value)
     let summe = parseInt(document.querySelector("#summe").value)
@@ -62,12 +61,11 @@ function faelligskeitsdarlehen(){
     let rate = summe * (zinsen / laufzeit)
     let rategesammt = rate * laufzeit
     let zahlung = document.createElement("div")
-    let geszahlung = document.createElement("div")
+    let zahlungGes = document.createElement("div")
     zahlung.innerText = "Monatliche Rate: " + rate.toFixed(2) + " €"
-    geszahlung.innerHTML = "Gesammte Zinsbelastung: " + rategesammt.toFixed(2) + " €"
+    zahlungGes.innerHTML = "Gesammte Zinsbelastung: " + rategesammt.toFixed(2) + " €"
     ratendiv.appendChild(zahlung)
-    ratendiv.appendChild(geszahlung)
-
+    ratendiv.appendChild(zahlungGes)
 }
 
 
